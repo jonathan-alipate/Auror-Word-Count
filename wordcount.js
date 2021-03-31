24dd19479f8a265876f78da5b2d6479880491d2e
Jonathan Alipate <j.alipate@gmail.com>
2021-04-01 09:54:50 +1300

    object word count working

--- wordcount.js
+++ wordcount.js
@@ -1,4 +1,15 @@
 //this function accepts a string of words and returns either the single most common word or an array of them if their are multiple.
 function findMostCommonWord(sentence){
-
+    const words = sentence.split(" ")
+    const uniqueWords = {}
+    words.map(word => {
+        if(word in uniqueWords){
+            uniqueWords[word] += 1
+        } else {
+            uniqueWords[word] = 1
+        }
+    })
+    console.log(uniqueWords)
 }
+
+findMostCommonWord('the cat jumped over the hat')
